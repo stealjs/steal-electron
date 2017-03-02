e46dfb75cd69ee24cc399de76039fe3462096676
Matthew Phillips <matthew@matthewphillips.info>
2017-03-02 11:55:49 -0500

    Got the demo to work

--- demo/build.js
+++ demo/build.js
@@ -4,14 +4,23 @@ var stealTools = require("steal-tools");
 
 var nwOptions = {
 	buildDir: './build',
-	platforms: ['osx'],
-	files: ["./**/*"]
+	platforms: ['darwin'],
+	files: ["dist/**/*", "production.html"]
+	//files: ["./**/*"]
 };
 
 var buildPromise = stealTools.build({
 	config: __dirname + "/package.json!npm"
+}, {
+	minify: false
 });
 
 buildPromise.then(function(buildResult){
 	return stealNw(nwOptions, buildResult);
+})
+.then(function(){
+	console.log("Build successful");
+})
+.catch(function(err){
+	console.error(err);
 });
